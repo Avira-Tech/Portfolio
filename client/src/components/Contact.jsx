ca2eda4204dfbad041ccb4bce4225b1d70a591ee
Avira Tech <avira.tech@zohomail.in>
2026-01-04 10:57:13 +0530

    Update Contact.jsx

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
     setStatus('submitting');
     
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://portfolio-backend-3p35.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
